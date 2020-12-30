ece22a0138a2db9bdfc868ead0844635d0893173
Daniel <visual.eyes.success@gmail.com>
2020-12-30 03:34:58 +0100

    updated r package start file
    installed roxygen2 and devtools

--- r_packages_start_code.R
+++ r_packages_start_code.R
@@ -1,7 +1,10 @@
-library(roxygen2)
+library(roxygen2);library(devtools)
 #readfiles
 #loop for files
 #climate models
 #transform pr to mm
 #change seconds to hours
 #group by year
+#rmarkdown
+#disintegrate blocks
+#function define
